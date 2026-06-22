5ed51351f8995bf4c57e30bdd929c34c37df4fb8
Hasan Md Zakaria <zakariak4@gmail.com>
2026-06-22 12:10:15 +0600

    use proxy for route protected

---
+++ proxy.js
@@ -0,0 +1,24 @@
+import { auth } from '@/app/lib/auth'
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!session) {
+        return NextResponse.redirect(new URL('/auth/login', request.url))
+    }
+    return NextResponse.next()
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request) { ... }
+
+export const config = {
+    matcher: [
+        `/dashboard/:path*`,
+        `/profile/:path*`,
+    ],
+}
